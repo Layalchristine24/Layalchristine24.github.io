363bf445e284b1f309efccd55f7655fd6487ea39
LayalChristine <layal.lettry@gmail.com>
2022-04-28 14:49:02 +0200

    website ok

---
+++ starter_cmd.R
@@ -0,0 +1,13 @@
+if (!requireNamespace("devtools")) install.packages("devtools")
+devtools::install_github("rstudio/blogdown")
+
+blogdown::install_hugo()
+
+library(blogdown)
+new_site() # default theme is lithium
+# need to stop serving so can use the console again
+install_theme("themes/hugo-lithium", 
+              theme_example = TRUE, 
+              update_config = TRUE)
+
+blogdown::serve_site() 
